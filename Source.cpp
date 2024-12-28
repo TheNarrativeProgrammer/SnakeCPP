188ddf2e224910499820280a92888b268ee24607
pg27jacob <pg27jacob@vfs.com>
2024-12-28 09:49:39 -0800

    GAME V1.1

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-
-int main()
+#include "Snake.h"
+// Main game loop
+int main() 
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Itsa Me!\n";
-    std::cout << "Branch Push\n";
+    Setup();                                                    // Initialize game variables
+    while (!gameOver) {                                         // Run the game until the player loses
+        Draw();                                                 // Display the game board
+        Input();                                                // Handle user input
+        Logic();                                                // Update the game state
+        Sleep(100);                                             // Slow down the game loop for better playability
+    }
+    return 0;                                                   // Exit the program
 }
